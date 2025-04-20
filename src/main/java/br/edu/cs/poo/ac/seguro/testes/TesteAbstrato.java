f4c0b0ea82617f6f1feaf430a5a44dac4b155c7f
Pedro de Araújo Duarte Gusmão <168600233+pedroguswander@users.noreply.github.com>
2025-04-20 17:10:28 -0300

    [ADD] Esqueleto das classes Mediator sem implementação

--- src/main/java/br/edu/cs/poo/ac/seguro/testes/TesteAbstrato.java
+++ src/main/java/br/edu/cs/poo/ac/seguro/testes/TesteAbstrato.java
@@ -5,25 +5,19 @@ import java.io.File;
 import org.junit.jupiter.api.BeforeEach;
 
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
-import br.edu.cs.poo.ac.seguro.daos.SeguradoEmpresaDAO;
-import br.edu.cs.poo.ac.seguro.entidades.SeguradoEmpresa;
 
-public abstract class TesteDAO extends TesteAbstrato {
+public abstract class TesteAbstrato {
     protected CadastroObjetos cadastro;
 
     protected abstract Class getClasse();
 
-    protected TesteDAO() {
+    protected TesteAbstrato() {
         cadastro = new CadastroObjetos(getClasse());
     }
 
     @BeforeEach
     public void setUp() {
         String sep = File.separator;
-        File dir = new File("." + sep + getClasse().getSimpleName());
-        File[] files = dir.listFiles();
-        for (File file : files) {
-            file.delete();
-        }
+        FileUtils.limparDiretorio("." + sep + getClasse().getSimpleName());
     }
 }
