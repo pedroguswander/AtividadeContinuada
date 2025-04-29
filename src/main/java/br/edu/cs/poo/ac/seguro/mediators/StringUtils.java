cdc6c6e134731d73ef308cfdfe5804694ff5946d
Pedro de Araújo Duarte Gusmão <168600233+pedroguswander@users.noreply.github.com>
2025-04-29 07:46:13 -0300

    [ADD] implementação de StringUtils e ValidorCpfCnpj

--- src/main/java/br/edu/cs/poo/ac/seguro/mediators/StringUtils.java
+++ src/main/java/br/edu/cs/poo/ac/seguro/mediators/StringUtils.java
@@ -3,9 +3,18 @@ package br.edu.cs.poo.ac.seguro.mediators;
 public class StringUtils {
     private StringUtils() {}
     public static boolean ehNuloOuBranco(String str) {
+        if (str == null)
+        {
+            return true;
+        }
         return false;
     }
     public static boolean temSomenteNumeros(String input) {
-        return false;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) < 48 || input.charAt(i) > 57) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/br/edu/cs/poo/ac/seguro/mediators/ValidadorCpfCnpj.java
+++ src/main/java/br/edu/cs/poo/ac/seguro/mediators/ValidadorCpfCnpj.java
@@ -1,10 +1,17 @@
 package br.edu.cs.poo.ac.seguro.mediators;
 
 public class ValidadorCpfCnpj {
+
     public static boolean ehCnpjValido(String cnpj) {
+        if (cnpj.length() == 14 && StringUtils.temSomenteNumeros(cnpj)) {
+            return true;
+        }
         return false;
     }
     public static boolean ehCpfValido(String cpf) {
+        if (cpf.length() == 11 && StringUtils.temSomenteNumeros(cpf)) {
+            return true;
+        }
         return false;
     }
 }
